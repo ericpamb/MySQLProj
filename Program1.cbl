b89f57e7573b64c02f1ea315aa17822599a273bf
Eric Pambid <eric.pambid@dxc.com>
2021-07-28 07:33:22 +0000

    Test Commit

--- Program1.cbl
+++ Program1.cbl
@@ -11,6 +11,7 @@
       *>Test Branch Collab 07/28/2021
       *>Test Pull from Local
       *>Testbranch6 Test LOCAL CHECKOUT Test Changes
+      *>STAGE Test Test Test
       
            EXEC SQL INCLUDE SQLCA  END-EXEC. 
        
